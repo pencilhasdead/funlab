55aee8e117859c434b5205ef86f48fe229acdb1d
squall wang <squall1989@126.com>
2021-09-17 19:14:27 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import styles from './index.less';
+
+export default function IndexPage() {
+
+
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
