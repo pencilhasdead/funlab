0732e7c83f5c15c180d356d33015da092c590c36
squall wang <squall1989@126.com>
2021-09-18 18:59:27 +0800

    ts实验

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,17 @@ import styles from './index.less';
 
 export default function IndexPage() {
 
+  interface IIndex {
+    count: number;
+  }
+
+  const [index, setState] = useState<IIndex>({
+    count: 1,
+  })
 
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      {index.count}
     </div>
   );
 }
