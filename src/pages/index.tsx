ef5a53c20ff866fae0f1bc66ec0053c729b18086
squall wang <squall1989@126.com>
2021-09-29 19:03:37 +0800

    初始化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './index.less';
+import icon from '../assets/image/icon_main.png'
 
 export default function IndexPage() {
 
@@ -13,7 +14,19 @@ export default function IndexPage() {
 
   return (
     <div>
-      {index.count}
+      <div className={styles.index_header}>
+        <img src={icon} alt='icon' />
+        <div className={styles.index_title}>
+          <span className={styles.title}>
+            FunLab
+          </span>
+          <span className={styles.des}>搞些没用但快乐的事~</span>
+        </div>
+      </div>
+      <div className={styles.index_wrapper}>
+        123
+      </div>
     </div>
+
   );
 }
